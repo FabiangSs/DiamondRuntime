bed0b0b113cc42cabe868497dc463ded62e120bc
neoapps-dev <asd22.info@gmail.com>
2026-08-09 02:17:32 +0300

    feat: prepare for Emerald

--- app/app/src/main/java/dev/lcehub/emerald/core/FileUtils.java
+++ app/app/src/main/java/dev/lcehub/emerald/core/FileUtils.java
@@ -410,7 +410,7 @@ public abstract class FileUtils {
         Intent intent;
         if (path.startsWith("file://")) {
             File file = new File(Uri.decode(path.replace("file://", "")));
-            intent = new Intent(Intent.ACTION_VIEW, FileProvider.getUriForFile(activity, "dev.lcehub.emerald.FileProvider", file));
+            intent = new Intent(Intent.ACTION_VIEW, FileProvider.getUriForFile(activity, activity.getPackageName()+".fileprovider", file));
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         }
         else intent = new Intent(Intent.ACTION_VIEW, Uri.parse(path));
